2656312f4bc30feb023579067d6d3493ed4f007e
Mali-121 <74376646+Mali-121@users.noreply.github.com>
2022-06-10 19:26:52 +0800

    new changes made

--- Chatbot-main/main.js
+++ Chatbot-main/main.js
@@ -6,76 +6,44 @@ const PERSON_IMG = "user.png";
 const BOT_NAME = "BOT";
 const PERSON_NAME = "Guest";
 const prompts = [
-  ["hi", "hey", "hello", "good morning", "good afternoon", "Bob"],
-  ["how are you?", "how is life?", "how are things?"],
-  ["what are you doing?", "what is going on", "what is up"],
-  ["how old are you"],
-  ["who are you", "are you human", "are you bot", "are you human or bot"],
-  ["who created you", "who made you"],
-  [
-    "your name please",
-    "your name",
-    "may i know your name",
-    "what is your name",
-    "what call yourself"
-  ],
-  ["i love you"],
-  ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
-  ["bad", "bored", "tired"],
-  ["help me", "tell me story", "tell me joke"],
-  ["ah", "yes", "ok", "okay", "nice"],
-  ["bye", "good bye", "goodbye", "see you later"],
-  ["what should i eat today"],
-  ["bro"],
-  ["what", "why", "how", "where", "when"],
-  ["no", "not sure", "maybe", "no thanks"],
-  [""],
-  ["haha", "ha", "lol", "hehe", "funny", "joke"]
-]
-const replies = [
-  ["Hello!", "Hi!", "Hey!", "Hi there!", "Howdy", "Fine. Be that way"],
-  [
-    "Fine... how are you?",
-    "Pretty well, how are you?",
-    "Fantastic, how are you?"
-  ],
-  [
-    "Nothing much",
-    "About to go to sleep",
-    "Can you guess?",
-    "I don't know actually",
-    "Sure",
-    "Calm down, I know what I'm doing!"
-  ],
-  ["I am infinite"],
-  ["I am just a bot", "I am a bot. What are you?", "whatever"],
-  ["Sure"],
-  ["I am nameless", "I don't have a name"],
-  ["I love you too", "Me too"],
-  ["Have you ever felt bad?", "Glad to hear it"],
-  ["Why?", "Why? You shouldn't!", "Try watching TV"],
-  ["What about?", "Once upon a time..."],
-  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
-  ["Bye", "Goodbye", "See you later"],
-  ["Sushi", "Pizza"],
-  ["Bro!"],
-  ["Great question"],
-  ["That's ok", "I understand", "What do you want to talk about?"],
-  ["Please say something :("],
-  ["Haha!", "Good one!"]
+  ['hi'],
+
+  ["how are you doing"],
+
+  ["bob"],
+
+  ["what are you doing"],
+
+  [''],
+
+  ['hello', 'where are you from', 'who are you', 'what can you do for me']
+
 ];
+const replies = [
+  ["hey"],
+
+  [
+    'sure',
+  ],
+
+  ["Whoa, chill out!"],
+
+  ["Calm down, I know what i am doing!"],
+
+  ["Fine. Be that way!"],
+
+  ["Whatever"],
+];
+/*
 const alternative = [
-  "Same",
-  "Go on...",
-  "Bro...",
-  "Try again",
-  "I'm listening...",
-  "I don't understand :/"
+  "Sure",
+  "Whoa, chill out!",
 ]
+*/
 const robot = ["How do you do, fellow human", "I am not a bot"];
 msgerForm.addEventListener("submit", event => {
   event.preventDefault();
-  const msgText = msgerInput.value;
+  const msgText = msgerInput.value.toString();
   if (!msgText) return;
   msgerInput.value = "";
   addChat(PERSON_NAME, PERSON_IMG, "right", msgText);
@@ -91,6 +59,7 @@ function output(input) {
     .replace(/please /g, "")
     .replace(/ please/g, "")
     .replace(/r u/g, "are you");
+
   if (compare(prompts, replies, text)) {
     product = compare(prompts, replies, text);
   } else if (text.match(/thank/gi)) {
